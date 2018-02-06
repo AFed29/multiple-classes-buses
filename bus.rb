a83a224a249f8846a8db38545172d6ea40f52ffc
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-02-06 14:32:20 +0000

    part B completed

--- bus.rb
+++ bus.rb
@@ -1,12 +1,30 @@
 class Bus
-  attr_reader(:number, :destination)
+  attr_reader(:number, :destination, :passengers)
 
-  def initialize(number, destination)
+  def initialize(number, destination, passengers = [])
     @number = number
     @destination = destination
+    @passengers = passengers
   end
 
   def drive
     return "Brum brum"
   end
+
+  def number_of_passengers()
+    return @passengers.length()
+  end
+
+  def pick_up(person)
+    @passengers.push(person)
+  end
+
+  def drop_off(person)
+    @passengers.delete_if { |passenger| passenger == person }
+  end
+
+  def empty()
+    @passengers.clear()
+  end
+
 end
--- specs/bus_specs.rb
+++ specs/bus_specs.rb
@@ -1,9 +1,15 @@
 require('minitest/autorun')
 require_relative('../bus')
+require_relative('../person')
 
 class BusTest < MiniTest::Test
   def setup()
-    @bus = Bus.new(16, "Colinton")
+
+    @person1 = Person.new("Alex", 26)
+    @person2 = Person.new("Jane", 41)
+    @person3 = Person.new("Kate", 21)
+
+    @bus = Bus.new(16, "Colinton", [@person1, @person2])
   end
 
   def test_get_bus_number
@@ -18,6 +24,32 @@ class BusTest < MiniTest::Test
 
   def test_bus_drive
     assert_equal("Brum brum", @bus.drive())
+  end
+
+  def test_number_of_passengers_on_bus
+    number_of_passengers = @bus.number_of_passengers()
+    assert_equal(2, number_of_passengers)
+  end
+
+  def test_pick_up_passenger
+    @bus.pick_up(@person3)
+    number_of_passengers = @bus.number_of_passengers()
+    assert_equal(3, number_of_passengers)
+    passengers = @bus.passengers()
+    assert_equal(@person3, passengers[2])
+  end
+
+  def test_drop_off_passenger
+    @bus.drop_off(@person2)
+    number_of_passengers = @bus.number_of_passengers()
+    assert_equal(1, number_of_passengers)
+    passengers = @bus.passengers()
+    assert_equal([@person1], passengers)
+  end
+
+  def test_empty_bus
+    @bus.empty()
+    assert_equal(0, @bus.number_of_passengers())
   end
 
 end
